a94c380376e0093d8ceba89ad5805338dd704205
Siddhant <84025779+methlox@users.noreply.github.com>
2023-07-15 01:37:40 +0530

    navbar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Providers } from "./providers";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'gm',
-}
+  title: "gm",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
